363150388988a4fa0290632f997c5e348399e034
Wasim Akaram <55456khanahmad@gmail.com>
2023-01-06 10:24:28 +0530

    I've done both Problems

---
+++ isPalindrome.js
@@ -0,0 +1,6 @@
+let str = "racecar";
+let bag = ""
+for(let i = str.length-1; i>=0; i--){
+  bag += str[j];
+}
+(bag == str)?console.log(str,"is a Palindrome"):console.log(str,"Not a Palindrome")
---
+++ isPrime.js
@@ -0,0 +1,8 @@
+let num = 7; 
+let count = 0;
+for(let i = 2; i<=num; i++){
+  if(num%j==0){
+    count++;
+  }
+}
+(count==1)?console.log(num,"is a Prime number"):console.log(num,"is not a Prime number");
