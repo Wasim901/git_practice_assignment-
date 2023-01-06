cbf68448c2fd300da5b54b8db968c7ea8fecff6e
unknown <ilmatahir1234@gmail.com>
2023-01-06 14:40:19 +0530

    Changes done.

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,6 +1,6 @@
 let str = "racecar";
 let bag = ""
 for(let i = str.length-1; i>=0; i--){
-  bag += str[j];
+  bag += str[i];
 }
 (bag == str)?console.log(str,"is a Palindrome"):console.log(str,"Not a Palindrome")
